18f00bb464f385410bd65c4877c9686889f56cd3
stiliyan-gospodinov <stelko095@gmail.com>
2024-04-09 22:14:47 +0300

    first

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { ApiService } from '../api.service';
 import { Comment } from '../types/comment';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comments',
@@ -13,58 +15,88 @@ import { DatePipe } from '@angular/common';
 })
 export class CommentsComponent implements OnInit {
   commentForm: FormGroup;
-  comment: string; 
   comments: Comment[] = [];
   newCommentPosted: boolean = false;
   newCommentId: string | undefined;
-  constructor(private formBuilder: FormBuilder,private userService: UserService, private apiService: ApiService, private datePipe: DatePipe) {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private apiService: ApiService,
+    private datePipe: DatePipe
+  ) {
     this.commentForm = this.formBuilder.group({
-      comment: ['', Validators.required] // Добавете валидатор за задължително поле
+      comment: ['', Validators.required]
     });
-    this.comment = this.commentForm.value.comment;
   }
 
   get isLogged(): boolean {
     return this.userService.isLogged;
   }
+
   get username(): string {
     return this.userService.user?.username || '';
   }
   
   ngOnInit(): void {
-    this.apiService.getComments().subscribe((data: Comment[]) => {
-      console.log(data);
-      this.comments = data;
-    });
+    this.loadComments();
   }
+
+  loadComments(): void {
+    this.apiService.getComments()
+      .pipe(
+        catchError(error => {
+          console.error('Error loading comments:', error);
+          return [];
+        })
+      )
+      .subscribe((data: Comment[]) => {
+        console.log(data);
+        this.comments = data;
+      });
+  }
+
   addComment(event: Event): void {
     event.preventDefault();
     
-    // Прочитане на стойността на коментара само при извикване на метода addComment()
     const comment = this.commentForm.value.comment;
 
     const formattedDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd HH:mm:ss');
     const newComment: Comment = {
-        name: this.username,
-        img: 'https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=',
-        comment: comment, // Използвайте стойността на коментара, който сте прочели от формата
-        posted: formattedDate || ''
+      name: this.username,
+      img: 'https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=',
+      comment: comment,
+      posted: formattedDate || ''
     };
 
-    this.apiService.postComment(newComment).subscribe((id:string) => {
+    this.apiService.postComment(newComment)
+      .pipe(
+        catchError(error => {
+          console.error('Error posting comment:', error);
+          throw error;
+        })
+      )
+      .subscribe((id: string) => {
         newComment.id = id; 
         this.comments.push(newComment);
-        this.commentForm.reset(); // Нулиране на стойността на формата
+        this.commentForm.reset();
         this.newCommentPosted = true;
         this.newCommentId = id;
-    });
-}
+      });
+  }
+
   closeNewComment(commentId: string | undefined): void {
     if (commentId) {
-      this.apiService.deleteComment(commentId).subscribe(() => {
-        // Изтриваме коментара от локалния списък
-        this.comments = this.comments.filter(comment => comment.id !== commentId);
-      });
+      this.apiService.deleteComment(commentId)
+        .pipe(
+          catchError(error => {
+            console.error('Error deleting comment:', error);
+            throw error;
+          })
+        )
+        .subscribe(() => {
+          this.comments = this.comments.filter(comment => comment.id !== commentId);
+        });
     }
     this.newCommentPosted = false;
   }
--- src/app/discount/discount.component.ts
+++ src/app/discount/discount.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Discount } from '../types/discount';
 import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-discount',
@@ -9,21 +10,33 @@ import { ApiService } from '../api.service';
   styleUrls: ['./discount.component.css']
 })
 export class DiscountComponent implements OnInit {
-  discountUser: Discount| undefined;
-  discountGuest: Discount| undefined;
+  discountUser: Discount | undefined;
+  discountGuest: Discount | undefined;
+
   constructor(private userService: UserService, private apiService: ApiService) {}
 
   get isLogged(): boolean {
     return this.userService.isLogged;
   }
+
   ngOnInit(): void {
-    this.apiService.getDiscountUser().subscribe((data: Discount | undefined) => {
-      console.log(data);
-      this.discountUser = data;
-    });
-    this.apiService.getDiscountGuest().subscribe((data: Discount | undefined) => {
-      console.log(data);
-      this.discountGuest = data;
-    });
-}
+    this.loadDiscountUser();
+    this.loadDiscountGuest();
+  }
+
+  loadDiscountUser(): void {
+    this.apiService.getDiscountUser()
+      .subscribe((data: Discount | undefined) => {
+        console.log(data);
+        this.discountUser = data;
+      });
+  }
+
+  loadDiscountGuest(): void {
+    this.apiService.getDiscountGuest()
+      .subscribe((data: Discount | undefined) => {
+        console.log(data);
+        this.discountGuest = data;
+      });
+  }
 }
